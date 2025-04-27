9229c8c1624af2187d2902ff5024dc737f6c0fd6
lombardlea <lealombard@epfl.ch>
2025-04-27 19:49:36 +0200

    création de l'application

---
+++ Lea's/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+# Titre de l'application
+st.title("Green solvent")
+
+# Demander à l'utilisateur d'entrer le SMILES du produit
+smiles_input = st.text_input("Enter the SMILES of the reaction product", "")
+
+if smiles_input:
+    # Convertir le SMILES en molécule
+    mol = Chem.MolFromSmiles(smiles_input)
+    
+#    if mol:
+#        # Dessiner la molécule
+#        img = Draw.MolToImage(mol)
+#        st.image(img, caption="Chemical structure of the product")
+        
+        # Afficher des informations supplémentaires sur la molécule
+#        st.write("Molecule Information:")
+        
+        # Exemple : afficher la masse molaire
+#        mol_weight = Chem.Descriptors.MolWt(mol)
+#        st.write(f"Molar weight : {mol_weight:.2f} g/mol")
+        
+        # Afficher la formule brute
+#        formula = Chem.rdMolDescriptors.CalcMolFormula(mol)
+#        st.write(f"Molecular formula : {formula}")
+#    else:
+#        st.write("The entered SMILES is not valid.")
